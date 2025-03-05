aa4dded864d17ae8ec2a1dd44d99e1553f2dc657
DivyaRajX <varshneydivya251707@gmail.com>
2025-03-05 15:00:41 +0530

    Day 1

---
+++ subSequence.java
@@ -0,0 +1,16 @@
+public class subSequence {
+    public static void main(String[] args) {
+        String str = "abc";
+        String ans = "";
+        sub(str,ans);
+    }
+    public static void sub(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        sub(str.substring(1), ans);
+        sub(str.substring(1), ans+ch);
+    }
+}
